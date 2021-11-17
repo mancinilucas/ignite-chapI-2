969e751a4ca66b309c218d60c6ed9342e0bbf04c
mancinilucas <mancini.lucasg@gmail.com>
2021-11-17 20:22:00 -0300

    desafio I.02

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,39 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from "react";
+import { GenreResponseProps } from "../App";
+import { Button } from "./Button";
+
+interface SideBarProps{
+  genres: GenreResponseProps[];
+  onChangeSelectedGenreId: (id: number) => void;
+}
+
+export function SideBar(props: SideBarProps) {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  useEffect(() => {
+    props.onChangeSelectedGenreId(selectedGenreId);
+  }, [selectedGenreId]);
+
+  function handleClickButton(id: number){
+    setSelectedGenreId(id);
+  }
+
+  return(
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {props.genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
 }
